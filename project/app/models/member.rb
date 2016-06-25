98fd48a0fb6d7dcf9e27f6223fe704841ca65003
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-06-24 23:25:02 -0400

    minor changes to page appearance

--- project/app/models/member.rb
+++ project/app/models/member.rb
@@ -1,11 +1,19 @@
 class Member
-  attr_accessor :sell, :bought, :sold, :rating, :bio
+  attr_accessor :sell, :bought, :sold, :buyrating, :sellrating, :bio
+
+  private
   def add_sell
   end
 
+  private
   def add_cart
   end
 
+  public
   def change_rating
+  end
+
+  public
+  def display_info
   end
 end
