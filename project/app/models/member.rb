700564fe575b5dab2a4544404b8f1c9c90e00ff2
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-06-25 21:22:31 -0400

    work on member class

--- project/app/models/member.rb
+++ project/app/models/member.rb
@@ -1,19 +1,29 @@
 class Member
-  attr_accessor :sell, :bought, :sold, :buyrating, :sellrating, :bio
+  attr_accessor :sell, :bought, :sold, :buyrating, :sellrating, :bio, :asks
 
   private
-  def add_sell
+  def add_sell()
   end
 
   private
-  def add_cart
+  def add_ask(newAsk)
+    asks << newAsk
+  end
+
+  private
+  def finish_ask(req)
+    asks.delete(req)
+  end
+
+  private
+  def add_cart()
   end
 
   public
-  def change_rating
+  def change_rating()
   end
 
   public
-  def display_info
+  def display_info()
   end
 end
