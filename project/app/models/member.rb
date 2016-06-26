b0aa9bcf2f61362c04cb7367ca30070d78c7d4dc
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-06-25 21:39:36 -0400

    edited member methods

--- project/app/models/member.rb
+++ project/app/models/member.rb
@@ -1,18 +1,30 @@
 class Member
-  attr_accessor :sell, :bought, :sold, :buyrating, :sellrating, :bio, :asks
+  attr_accessor :sell, :bought, :sold, :buyrating, :sellrating, :bio, :reqtofill
 
-  private
-  def add_sell()
+  def new(name)
+    @name = name
+    @sellrating = 0
+    @buyrating = 0
+    @sell = Array.new
+    @bought = Array.new
+    @sold = Array.new
+    @bio = ""
+    @reqtofill = Array.new
   end
 
   private
-  def add_ask(newAsk)
-    asks << newAsk
+  def add_sell(item)
+    @sell << item
   end
 
   private
-  def finish_ask(req)
-    asks.delete(req)
+  def add_req(newAsk)
+    @reqtofill << newAsk
+  end
+
+  private
+  def finish_req(req)
+    @reqtofill.delete(req)
   end
 
   private
@@ -20,10 +32,17 @@ class Member
   end
 
   public
-  def change_rating()
+  def change_srating(num)
+    @sellrating += num
   end
 
   public
-  def display_info()
+  def change_brating(num)
+    @buyrating += num
+  end
+
+  public
+  def display_info
+    return [@sellrating, @sell, @sold]
   end
 end
